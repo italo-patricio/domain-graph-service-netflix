a0224a93737234ca1202780ccc2f98022e074cfa
Italo Patricio <login+italo-patricio@elo7.com>
2021-05-24 18:18:27 -0300

    resolve busca com async dataloader

--- mproduct/src/main/kotlin/br/com/italopatricio/grapqhl/ProductDataLoader.kt
+++ mproduct/src/main/kotlin/br/com/italopatricio/grapqhl/ProductDataLoader.kt
@@ -5,6 +5,7 @@ import org.dataloader.BatchLoader
 import org.springframework.beans.factory.annotation.Autowired
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
+import java.util.stream.Collectors
 
 
 @DgsDataLoader(name = "productsOfBatchDataLoader")
@@ -15,7 +16,10 @@ class ProductBatchLoader: BatchLoader<Int, Product> {
 
     override fun load(keys: List<Int>): CompletionStage<List<Product>>? {
         println("ProductBatchLoader.load")
-        return CompletableFuture.supplyAsync { return@supplyAsync productService.getProductsBySellersId(keys) }
+        return CompletableFuture.supplyAsync { keys.map {
+             productService.getProductsBySellerId(it)
+            }.toList() as List<Product>
+        }
     }
 
 }
